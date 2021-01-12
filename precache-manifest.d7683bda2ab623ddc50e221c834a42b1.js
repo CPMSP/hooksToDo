1bc03b2e81cedf3dcd539772893757856030070f
CPMSP <cpetersonmsp@gmail.com>
2021-01-12 00:03:00 -0600

    Updates

---
+++ precache-manifest.d7683bda2ab623ddc50e221c834a42b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f448baf14d03923242769a4b9a469469",
+    "url": "/hooksToDo/index.html"
+  },
+  {
+    "revision": "e8f8b2dac38e5a9e455b",
+    "url": "/hooksToDo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "92e29565f7eca5eae6ba",
+    "url": "/hooksToDo/static/js/2.beb39c45.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hooksToDo/static/js/2.beb39c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f8b2dac38e5a9e455b",
+    "url": "/hooksToDo/static/js/main.70511abd.chunk.js"
+  },
+  {
+    "revision": "015fb0be0d023d33a06e",
+    "url": "/hooksToDo/static/js/runtime-main.85298572.js"
+  }
+]);
